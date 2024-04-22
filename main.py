41e80b975366f82a5ff92cd9cb8ab3bdecc2f135
LunaXCBN <basikallyluna@gmail.com>
2024-04-22 12:53:41 +0300

    Add loop control commands. Add more info to embed.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
+import datetime
+import time
 from discord.ext import commands, tasks
 from sourceserver.sourceserver import SourceServer
-from time import sleep
+
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix="=", intents=intents, help_command=None)
@@ -18,28 +20,38 @@ def getinfo():
     return hostname, map, playercount
 
 
-@client.command()
-@commands.guild_only()
-@commands.is_owner()
-async def sync(ctx: commands.Context):
-    synced = await client.tree.sync()
-    await ctx.send(f"{len(synced)} commands synced")
-
-
 @client.command(description="Force update server status")
 @commands.is_owner()
-async def fupdate(ctx: commands.Context):
+async def start_loop(ctx: commands.Context):
     try:
         server_status.start(ctx)
     except RuntimeError:
-        server_status.restart(ctx)
+        await ctx.send("Loop already running.")
 
+@client.command()
+@commands.is_owner()
+async def stop_loop(ctx: None):
+    server_status.cancel()
+    await ctx.send("Stopped loop.")
 
-@tasks.loop(seconds=120)
+@client.command()
+@commands.is_owner()
+async def check_loop(ctx: commands.Context):
+    if server_status.is_running() == False:
+        await ctx.send("Loop not running.")
+    else:
+        current = server_status.current_loop
+        await ctx.send(f"Current iteration: {current}")
+
+@tasks.loop(minutes=2, reconnect=True)
 async def server_status(ctx: commands.Context):
     info = getinfo()
+    dnow = datetime.datetime.now()
+    dnow_format = dnow.strftime("%d.%m.%Y %H:%M:%S")
+    ctime = int(time.time())
+
     embed = discord.Embed(title="Server Status",
-                    description=f"{info[0]}",
+                    description=f"{info[0]}\nLast update: <t:{ctime}:R>",
                     colour=0xb450b1)
     embed.set_author(name="65.21.189.168:27015")
     embed.add_field(name="map:",
@@ -48,13 +60,15 @@ async def server_status(ctx: commands.Context):
     embed.add_field(name="players:",
                     value=f"{info[2]}/24",
                     inline=True)
-    embed.set_footer(text="Bot created by LunaXCBN | discord.py")
+    embed.set_footer(text="Is this wrong? Contact Luna.")
 
     global message
     if message is None:
         message = await ctx.send(embed=embed)
     else:
         await message.edit(embed=embed)
+
+    print(f"Last update: {dnow_format}")
 
 
 client.run("MTIyNTg3NzU4MDAyMDk3NzgyNg.Gsc7N_.OyuzoLetilab7j3ir2NZ60fQAPhNcXI-42mQt4")
